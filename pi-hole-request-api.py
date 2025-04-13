adebf78b0767ddd00e615be80581da4a1a72f94f
João Oliveira <joaoliveira9@outlook.com>
2025-04-13 17:38:54 -0300

    Update: testando o metodo get de blocking

--- pi-hole-request-api.py
+++ pi-hole-request-api.py
@@ -8,6 +8,17 @@ response = requests.request("POST", url, json=payload, verify=False)
 response =  response.json()
 """ print(response) """
 
-sid = response["session"]["sid"]
-print(sid)
+""" sid = response["session"]["sid"] """
+""" print(sid) """
 
+def teste():
+    sid = "DbyYZZHQhLL0RIc5yIx97A="
+    url = "https://192.168.15.2/api/dns/blocking?sid=" + sid
+    print(url)
+
+    response = requests.request("GET",url,verify=False)
+    print(response.json())
+
+""" teste(sid) """
+
+teste()
