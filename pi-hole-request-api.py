6dacc3004c848fd98233a84e36c8970d6e6a5c3a
João Oliveira <joaoliveira9@outlook.com>
2025-04-21 20:09:09 -0300

    Update: lista de querys com filtros funcionando

--- pi-hole-request-api.py
+++ pi-hole-request-api.py
@@ -57,39 +57,51 @@ def queryRegister(sid):
     print(response)
 
 """ queryRegister(sid) """
-
+password = "CI3ZjCva"
+pihole_address = "192.168.15.2"
 
 """ geração da sid e exclusao da sid """
 
 """ Cria uma sessao e envia """
 def create_session(password,pihole_address):
     url = "https://" + pihole_address + "/api/auth"
-    print(url)
+    """ print(url) """
     payload = {"password": password}
     response = requests.request("Post",url,json=payload,verify=False)
     response = response.json()
-    print(response)
+    """ print(response) """
     sid = response["session"]["sid"]
-    print(sid)
+    """ print(sid) """
     return sid
 
 
 def delete_session(sid,pihole_address):
     url = "https://" + pihole_address + "/api/auth?sid=" + sid
-    print(url)
+    """ print(url) """
     response = requests.request("Delete",url,verify=False)
     """ response = response.json() """
-    print(response)
+    """ print(response) """
 
+def queryRegister(pihole_address):
+    """ "https://pi.hole:443/api/queries?client_ip=192.168.15.5" """
+    sid = create_session(password,pihole_address)
+    url = "https://" + pihole_address + "/api/queries?client_ip=192.168.15.5&domain=x.com&length=4&sid="+ sid
+    response = requests.request("GET", url, json=payload, verify=False)
+    """ response =  response.json() """
+    print(response.json())
+    delete_session(sid,pihole_address)
+""" http://192.168.0.22/api/queries?client_ip=192.168.0.8&length=1 """
 
-password = "CI3ZjCva"
-pihole_address = "192.168.15.2"
+""" password = "CI3ZjCva"
+pihole_address = "192.168.15.2" """
 
+queryRegister(pihole_address)
 """ criando uma sessao """
-sid = create_session(password,pihole_address) 
-print(sid) 
+""" sid = create_session(password,pihole_address)  """
+""" print(sid)  """
 
-delete_session(sid,pihole_address)
+""" delete_session(sid,pihole_address) """
+
 
 """ queryRegister("opIVV0VHU7paj4YRDFXboA=") """
 
