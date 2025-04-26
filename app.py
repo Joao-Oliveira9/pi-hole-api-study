0c99b4953de1767e0a19d379453e86575983705b
João Oliveira <joaoliveira9@outlook.com>
2025-04-26 19:36:56 -0300

    Update: rota para bloqueio de dominios vinculado a um grupo

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
 from pi_hole_request_api import RequisicoesPihole
-from flask import Flask,request
+from flask import Flask,request,jsonify
 import requests
 
 pihole = RequisicoesPihole(password="CI3ZjCva", pihole_address="192.168.15.2")
 
-
-""" id = pihole.get_id_group("teste")
-
-print(id) """
 app = Flask(__name__)
-
-""" @app.route("/")
-def hello_world():
-        print('passei aqui')
-        pihole.remove_group('tee')
-        return 'hello word' """
 
 @app.route("/get_registro", methods=['GET'])
 def get_registro():
    data = request.get_json() 
    domain_name = data['domain-name']
    length = data['length']
-   """ print(data) """
-   """ print(length) """
    response = pihole.query_register(domain_name,length)
-
    return response
-  
+
+@app.route("/add_domain_blocklist",methods=['POST'])
+def post_domain():
+    data = request.get_json()
+    domain_name = data['domain-name']
+    group_name = data['group-name']
+    pihole.add_domain_blocklist(domain_name,group_name)
+
+    message = f'Domínio {domain_name} adicionado a lista de bloqueios do grupo {group_name}' 
+    return jsonify(message),200
 
 
 if __name__ == "__main__":    
